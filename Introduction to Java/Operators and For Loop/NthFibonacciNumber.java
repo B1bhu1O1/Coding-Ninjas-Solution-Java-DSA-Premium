58f6ab5f2f96106f72a553e2d508095b10e51d2a
Bibhudatta Mohanty <68940229+B1bhu1O1@users.noreply.github.com>
2022-08-14 08:27:18 +0530

    Create NthFibonacciNumber.java

---
+++ Introduction to Java/Operators and For Loop/NthFibonacciNumber.java
@@ -0,0 +1,53 @@
+/*
+Nth term of Fibonacci series F(n), where F(n) is a function, is calculated using the following formula -
+    F(n) = F(n-1) + F(n-2), 
+    Where, F(1) =  0, 
+           F(2) = 1
+Provided N you have to find out the Nth Fibonacci Number.
+Input Format :
+The first line of each test case contains a real number ‘N’.
+Output Format :
+For each test case, return its equivalent Fibonacci number.
+Constraints:
+1 <= N <= 10000     
+Where ‘N’ represents the number for which we have to find its equivalent Fibonacci number.
+
+Time Limit: 1 second
+Sample Input 1:
+6
+Sample Output 1:
+8
+Explanation of Sample Input 1:
+Now the number is ‘6’ so we have to find the “6th” Fibonacci number
+So by using the property of the Fibonacci series i.e 
+
+[ 1, 1, 2, 3, 5, 8]
+So the “6th” element is “8” hence we get the output.
+*/
+
+
+import java.util.Scanner;
+public class Solution {
+
+
+	public static void main(String[] args) {
+		
+		/* Your class should be named Solution.
+	 	* Read input as specified in the question.
+	 	* Print output as specified in the question.
+		*/
+
+		Scanner s=new Scanner(System.in);
+        int n=s.nextInt();
+        int a=0, b=1;
+        for(int i=1; i<=n; i++)
+        {
+           int c=a+b;
+            a=b;
+            b=c;
+        }
+        System.out.println(a);
+        s.close();
+	}
+
+}
