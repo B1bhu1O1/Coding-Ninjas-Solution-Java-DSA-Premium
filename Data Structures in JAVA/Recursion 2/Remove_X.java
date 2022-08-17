b4881e744dc51221aa735d66a5ea492d8c3a719b
Bibhudatta Mohanty <68940229+B1bhu1O1@users.noreply.github.com>
2022-08-17 17:25:31 +0530

    Create Remove_X.java

---
+++ Data Structures in JAVA/Recursion 2/Remove_X.java
@@ -0,0 +1,50 @@
+/*
+Given a string, compute recursively a new string where all 'x' chars have been removed.
+Input format :
+String S
+Output format :
+Modified String
+Constraints :
+1 <= |S| <= 10^3
+where |S| represents the length of string S. 
+Sample Input 1 :
+xaxb
+Sample Output 1:
+ab
+Sample Input 2 :
+abc
+Sample Output 2:
+abc
+Sample Input 3 :
+xx
+Sample Output 3:
+
+*/
+
+
+
+public class solution {
+
+	// Return the changed string
+	public static String removeX(String input){
+		// Write your code here
+
+         if(input.length()==0)
+        {
+            return input;
+        }
+        
+        String smallO = removeX(input.substring(1));
+        if(input.charAt(0)=='x')
+        {
+        	return smallO;
+        }
+        else
+        	return input.charAt(0)+smallO;
+        
+
+	}
+	public static void main(String[] args) {
+		System.out.println(removeX("xaxb"));
+	}
+}
