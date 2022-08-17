7ce1cc20c6cbfdd26d39d048a4b94e4370e35c6b
Bibhudatta Mohanty <68940229+B1bhu1O1@users.noreply.github.com>
2022-08-17 17:29:18 +0530

    Create Replace_Character_Recursively.java

---
+++ Data Structures in JAVA/Recursion 2/Replace_Character_Recursively.java
@@ -0,0 +1,38 @@
+/*
+Given an input string S and two characters c1 and c2, you need to replace every occurrence of character c1 with character c2 in the given string.
+Do this recursively.
+Input Format :
+Line 1 : Input String S
+Line 2 : Character c1 and c2 (separated by space)
+Output Format :
+Updated string
+Constraints :
+1 <= Length of String S <= 10^6
+Sample Input :
+abacd
+a x
+Sample Output :
+xbxcd
+*/
+
+
+
+public class Solution {
+
+	public static String replaceCharacter(String input, char c1, char c2) {
+		/* Your class should be named Solution
+		 * Don't write main().
+		 * Don't read input, it is passed as function argument.
+		 * Return output and don't print it.
+		 * Taking input and printing output is handled automatically.
+		 */
+        
+         if(input.length()==0)
+            return input;
+        if(input.charAt(0)==c1)
+            return c2+replaceCharacter(input.substring(1),c1,c2);
+        else
+            return input.charAt(0)+replaceCharacter(input.substring(1),c1,c2);
+
+	}
+}
