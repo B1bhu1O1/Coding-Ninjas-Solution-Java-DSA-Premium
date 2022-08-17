27bf60e413127c1b3710176f6356fda4e7d700f9
Bibhudatta Mohanty <68940229+B1bhu1O1@users.noreply.github.com>
2022-08-18 01:14:45 +0530

    Create Code_Find_Height.java

---
+++ Data Structures in JAVA/Trees/Code_Find_Height.java
@@ -0,0 +1,51 @@
+/*
+Given a generic tree, find and return the height of given tree.
+Input Format:
+The first line of input contains data of the nodes of the tree in level order form. The order is: data for root node, number of children to root node, data of each of child nodes and so on and so forth for each node. The data of the nodes of the tree is separated by space.  
+Output Format :
+The first and only line of output prints the height of the given generic tree.
+Constraints:
+Time Limit: 1 sec
+Sample Input 1:
+10 3 20 30 40 2 40 50 0 0 0 0 
+Sample Output 1:
+3
+*/
+
+
+public class Solution {
+
+	/*	TreeNode structure 
+	 * 
+	 * class TreeNode<T> {
+		T data;
+		ArrayList<TreeNode<T>> children;
+
+		TreeNode(T data){
+			this.data = data;
+			children = new ArrayList<TreeNode<T>>();
+		}
+	}*/
+
+	public static int getHeight(TreeNode<Integer> root){
+		/* Your class should be named Solution
+		 * Don't write main().
+		 * Don't read input, it is passed as function argument.
+		 * Return output and don't print it.
+		 * Taking input and printing output is handled automatically.
+		 */
+        
+        if (root == null)
+            return 0;
+
+        int max = 1;
+        for (TreeNode<Integer> child : root.children) {
+            int childHeight = 1 + getHeight(child);
+            if (max < childHeight)
+                max = childHeight;
+        }
+        return max;
+	}
+
+}
+
