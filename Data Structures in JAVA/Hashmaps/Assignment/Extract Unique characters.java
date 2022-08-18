f0f72bdebabee13baeda8b3e7bd3fae39f6ca94c
Bibhudatta Mohanty <68940229+B1bhu1O1@users.noreply.github.com>
2022-08-18 12:15:40 +0530

    Create Extract Unique characters.java

---
+++ Data Structures in JAVA/Hashmaps/Assignment/Extract Unique characters.java
@@ -0,0 +1,49 @@
+/*
+Given a string S, you need to remove all the duplicates. That means, the output string should contain each character only once. The respective order of characters should remain same, as in the input string.
+Input format:
+The first and only line of input contains a string, that denotes the value of S.
+Output format :
+The first and only line of output contains the updated string, as described in the task.
+Constraints :
+0 <= Length of S <= 10^8
+Time Limit: 1 sec
+Sample Input 1 :
+ababacd
+Sample Output 1 :
+abcd
+Sample Input 2 :
+abcde
+Sample Output 2 :
+abcde
+*/
+
+
+import java.util.HashMap;
+
+public class Solution {
+
+	public static String uniqueChar(String str){
+		/* Your class should be named Solution
+		 * Don't write main().
+		 * Don't read input, it is passed as function argument.
+		 * Return output and don't print it.
+	 	 * Taking input and printing output is handled automatically.
+		*/
+
+         HashMap<Character, Integer> map = new HashMap<>();
+        String s = "";
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            if (!map.containsKey(ch)) {
+                s = s + ch;
+                map.put(ch, 1);
+            }
+        }
+        return s;
+        
+	}
+}
+
+
+
+
